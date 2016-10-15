716ca048fbbb09e48bad1ad9eec6be634f65c895
Dean <gaffmasterflex123@gmail.com>
2016-10-15 16:36:47 +0100

    implemented power ups and power up effects, power up collision detection needs some work

--- code/pong.cpp
+++ code/pong.cpp
@@ -18,6 +18,9 @@
 Ball balls[BALL_MAX_COUNT];
 int ballCount;
 
+PowerUp powerUps[POWER_UP_MAX_COUNT];
+int powerUpCount;
+
 float paddle_x_pos;
 float paddle_y_pos;
 float paddle_speed;
@@ -29,10 +32,16 @@ int score = 0;							// number bounces off paddle
 int lives = 3;							// number of balls left to play						
 int auto_mode = 0;						// flag - true for computer playing
 										
-std::string powerUps[5] = {"","","","",""}; 			//max of five powerups at any one time
 
 // frame information
 double this_time, old_time, dt, start_time;
+
+//checks collision between balls and power ups
+bool checkBallCollisionWithPowerUp(Ball &ball, PowerUp &powerUp){
+	bool isCollidingOnX = ball.x + BALL_SIZE >= powerUp.x && powerUp.x + POWER_UP_SIZE >= ball.x;
+	bool isCollidingOnY = ball.y + BALL_SIZE >= powerUp.y && powerUp.y + POWER_UP_SIZE >= ball.y;
+	return isCollidingOnX && isCollidingOnY;
+}
 
 //clamps values of paddles length
 float clamp(float value,float max,float min){
@@ -40,6 +49,11 @@ float clamp(float value,float max,float min){
 	if(value > max)return max;
 	
 	return value;
+}
+
+//used for spawning power ups in random places within level boundary
+int randomNumber(int min, int max){
+	return (rand() % (max-min + 1) + min);
 }
 
 void start_life() {
@@ -56,36 +70,38 @@ void start_life() {
 	// initial paddle position
 	paddle_x_pos    = WINDOW_WIDTH - PADDLE_WIDTH/2;
 	paddle_y_pos    = WINDOW_HEIGHT/2;
-	paddleLength = PADDLE_MAX_LEN;
+	paddleLength = PADDLE_START_LEN;
 
 	//set score
 	score = 0;
 
-	//reset power up array
-	resetPowerUps(powerUps);
-	
+	//set powerup count
+	powerUpCount = 1;
 }
 
-
-void resetPowerUps(std::string []){
-	for(int i = 0; i < 5;i++){
-		powerUps[i] = "";
-	}
-}
-
+//calculates the chance of spawning a reward
 bool calculateChance(const int& chance){
 	const int random = rand() % chance;
 	return random == 0;
 }
 
+//triggers rewards
 void triggerReward(){
 	//1 in 15 to spawn a powerup
-	if(calculateChance(5)){
-		//put powerup into a vector or add to array (would need to limit power up array then)
-		spawnBall(WINDOW_WIDTH/2,WINDOW_HEIGHT/2,rand());
+	if(calculateChance(10)){
+		//spawn an extra ball
+		spawnBall(WINDOW_WIDTH/2,WINDOW_HEIGHT/2,0.962);
+		printf("Ball Spawned\n");
+	}else if(calculateChance(5)){
+		// spawn a power up
+		float x = (float)(randomNumber(BORDER_SIZE * 2, WINDOW_WIDTH - (BORDER_SIZE * 2)));
+		float y = (float)(randomNumber(BORDER_SIZE * 2, WINDOW_HEIGHT - (BORDER_SIZE * 2)));
+		spawnPowerUp(x,y);
+		printf("Power up Spawned\n");
 	}else{
 		//if no power up or trap spawned give player extra points
 		score += 2;
+		printf("Extra Points given\n");
 	}
 }
 
@@ -93,16 +109,40 @@ void spawnBall(float x,float y,float angle){
 	if(ballCount + 1 > BALL_MAX_COUNT)return;
 
 	ballCount++;						//increment ball count
-	balls[ballCount].x = x;				//set new ball fields
-	balls[ballCount].y = y;
-	balls[ballCount].angle = angle;
-	balls[ballCount].isActive = true;
+	balls[ballCount - 1].x = x;				//set new ball fields
+	balls[ballCount - 1].y = y;
+	balls[ballCount - 1].angle = angle;
+	balls[ballCount - 1].isActive = true;
 }
 
 void destroyBall(int index){
-			
-		balls[index].isActive = false;
-		ballCount--;
+	balls[index].isActive = false;
+	ballCount--;
+}
+
+void spawnPowerUp(float x, float y){
+	if (powerUpCount + 1 > POWER_UP_MAX_COUNT)return;
+
+	powerUpCount++;
+	powerUps[powerUpCount - 1].x = x;
+	powerUps[powerUpCount - 1].y = y;
+	powerUps[powerUpCount - 1].isActive = true;
+
+	int randomType = rand() % (POWER_UP_TYPE_MAX_COUNT - 0 + 1) + 0;
+	powerUps[powerUpCount - 1].type = powerUps[powerUpCount - 1].powers[randomType];
+}
+
+void destroyPowerUp(int index){
+	powerUps[index].isActive = false;
+	powerUpCount--;
+}
+
+//deals with applying power ups to the game
+void activatePowerUp(PowerUp &powerUp){
+	if(powerUp.type.compare("PADDLE_MAX"))paddleLength = PADDLE_MAX_LEN;
+	else if(powerUp.type.compare("PADDLE_MIN"))paddleLength = PADDLE_MIN_LEN;
+	else if(powerUp.type.compare("EXTRA_POINTS"))score+=10;
+	else if(powerUp.type.compare("DESTROY_BALL"))destroyBall(ballCount - 1);
 }
 
 void update() {
@@ -172,6 +212,22 @@ void update() {
 	 	}	
 	}
 }
+
+	//check if any balls hit a power up
+	for(int ball = 0; ball < ballCount; ball++){
+		for(int powerUp = 0; powerUp < powerUpCount;powerUp++){
+			if(checkBallCollisionWithPowerUp(balls[ball],powerUps[powerUp])){
+				printf("Ball hit power up\n");
+
+				//use power up power
+				activatePowerUp(powerUps[powerUp]);
+				printf("%s\n",powerUps[powerUp].type);
+				//destroy the power up then
+				destroyPowerUp(powerUp);
+				printf("PowerUp Destroyed\n");
+			}
+		}
+	}
 
  	//trigger something every 5th hit
  	if(score % 5 == 0 && score != 0){
